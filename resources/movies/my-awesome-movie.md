f9de3770ed8515ad19aeff73d612714461dd994c
durjoy1971 <durjoybarua1971@gmail.com>
2024-10-29 15:45:24 +0600

    Reading & Supporting Markdown

---
+++ resources/movies/my-awesome-movie.md
@@ -0,0 +1,3 @@
+# Welcome to My Movie
+
+This is a placeholder text for the movie page. More content will be added soon.
--- start/routes.ts
+++ start/routes.ts
@@ -7,20 +7,66 @@
 |
 */
 
+import { Exception } from '@adonisjs/core/exceptions'
 import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
 import fs from 'node:fs/promises'
+import { MarkdownFile } from '@dimerapp/markdown'
+import { toHtml } from '@dimerapp/markdown/utils'
 
 router.on('/').render('pages/home').as('home')
 
-//* Using Route Parameters -> :slug
+//* Reading and Supporting Markdown
 router
   .get('/movies/:slug', async (ctx) => {
-    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
-    const movie = await fs.readFile(url, 'utf-8')
-    return ctx.view.render('pages/movies/show', { movie })
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.md`)
+
+    try {
+      const file = await fs.readFile(url, 'utf-8')
+      const md = new MarkdownFile(file)
+      await md.process()
+      const movie = toHtml(md).contents
+
+      ctx.view.share({ movie })
+    } catch (error) {
+      throw new Exception('Could not find a move called ' + `${ctx.params.slug}`, {
+        code: 'E_NOT_FOUND',
+        status: 404,
+      })
+    }
+    return ctx.view.render('pages/movies/show')
   })
   .as('movies.show')
+  .where('slug', router.matchers.slug())
+
+//* Validate Route Parameters -> :slug
+// router
+//   .get('/movies/:slug', async (ctx) => {
+//     const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+//     let movie: any
+
+//     try {
+//       movie = await fs.readFile(url, 'utf-8')
+//       ctx.view.share({ movie })
+//     } catch (error) {
+//       throw new Exception('Could not find a move called ' + `${ctx.params.slug}`, {
+//         code: 'E_NOT_FOUND',
+//         status: 404,
+//       })
+//     }
+//     return ctx.view.render('pages/movies/show')
+//   })
+//   .as('movies.show')
+//   .where('slug', router.matchers.slug())
+
+//* Using Route Parameters -> :slug
+// router
+//   .get('/movies/:slug', async (ctx) => {
+//     const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+//     const movie = await fs.readFile(url, 'utf-8')
+//     return ctx.view.render('pages/movies/show', { movie })
+//   })
+//   .as('movies.show')
 
 //? router with default way
 // router
