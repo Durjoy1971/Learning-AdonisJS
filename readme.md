484e37e7e7eabf53d3f93d4ef1f6ec1a68a97b86
durjoy1971 <durjoybarua1971@gmail.com>
2024-10-30 11:27:13 +0600

    Routes with Controllers

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Services
+# <span style="color:teal">Services</span>
 
 Suppose you need to fetch user data and perform some transformations on it. Instead of writing this logic in multiple controllers, you can create a `UserService` with a method like `getUserData(userId)`.
 
@@ -16,8 +16,8 @@ class UserService {
 module.exports = new UserService();
 ```
 
-## Using the Service in a Controller
-
+### <span style="color:brown">Using the Service in a Controller</span>
+# 
 ```javascript
 // controllers/UserController.js
 const UserService = require('../services/UserService');
@@ -32,8 +32,39 @@ class UserController {
 module.exports = UserController;
 ```
 
-## Things We Got From Services
-
+### <span style="color:brown">Things We Got From Services</span>
+---
 - **Services** encapsulate reusable logic to keep controllers clean.
 - **Creating services** involves defining a class and using it in controllers.
 - **Benefits** include better code organization, easier testing, and maintaining separation of concerns.
+
+---
+# <span style="color:teal">Controller Basics</span>
+Controllers are responsible for handling incoming requests and returning responses to the client. They typically interact with services to perform business logic and return the result.
+
+```javascript
+// controllers/UserController.js
+class UserController {
+    async show({ params, response }) {
+        // Interact with the service to get user data
+        const userData = await UserService.getUserData(params.id);
+        return response.json(userData);
+    }
+}
+
+module.exports = UserController;
+```
+
+### <span style="color:brown">Routes with Controllers</span>
+
+Routes define the endpoints of your application and map them to controller actions. This allows you to handle different HTTP requests and direct them to the appropriate controller methods.
+
+```javascript
+// start/routes.js
+const Route = use('Route');
+const UserController = require('../app/Controllers/Http/UserController');
+
+Route.get('/users/:id', 'UserController.show');
+```
+
+By defining routes, you can ensure that incoming requests are handled by the correct controller actions, promoting a clean and organized codebase.
--- start/routes.ts
+++ start/routes.ts
@@ -8,80 +8,10 @@
 */
 
 import router from '@adonisjs/core/services/router'
-import { toHtml } from '@dimerapp/markdown/utils'
-import MovieService from '#services/movie_service'
+const MoviesController = () => import('#controllers/movies_controller')
 
+router.get('/', [MoviesController, 'index']).as('home')
 router
-  .get('/', async (ctx) => {
-    const slugs = await MovieService.getSlugs()
-    const movies: Record<string, any>[] = []
-
-    for (const slug of slugs) {
-      const md = await MovieService.read(slug)
-
-      movies.push({
-        title: md.frontmatter.Title,
-        slug,
-      })
-    }
-
-    return ctx.view.render('pages/home', { movies })
-  })
-  .as('home')
-
-//* Reading and Supporting Markdown
-router
-  .get('/movies/:slug', async (ctx) => {
-    const md = await MovieService.read(ctx.params.slug)
-    const movie = toHtml(md).contents
-    ctx.view.share({ movie, md })
-    return ctx.view.render('pages/movies/show')
-  })
+  .get('/movies/:slug', [MoviesController, 'show'])
   .as('movies.show')
   .where('slug', router.matchers.slug())
-
-//* Validate Route Parameters -> :slug
-// router
-//   .get('/movies/:slug', async (ctx) => {
-//     const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
-//     let movie: any
-
-//     try {
-//       movie = await fs.readFile(url, 'utf-8')
-//       ctx.view.share({ movie })
-//     } catch (error) {
-//       throw new Exception('Could not find a move called ' + `${ctx.params.slug}`, {
-//         code: 'E_NOT_FOUND',
-//         status: 404,
-//       })
-//     }
-//     return ctx.view.render('pages/movies/show')
-//   })
-//   .as('movies.show')
-//   .where('slug', router.matchers.slug())
-
-//* Using Route Parameters -> :slug
-// router
-//   .get('/movies/:slug', async (ctx) => {
-//     const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
-//     const movie = await fs.readFile(url, 'utf-8')
-//     return ctx.view.render('pages/movies/show', { movie })
-//   })
-//   .as('movies.show')
-
-//? router with default way
-// router
-//   .get('/movies/ok', async (ctx) => {
-//     return ctx.view.render('pages/movies', { movie: 'router-value 5' })
-//   })
-//   .as('movies.show')
-
-//* More Example
-// router.get('/movies', () => {}).as('movies.index')
-// router.get('/movies/ok', () => {}).as('movies.show')
-// router.get('/movies/create', () => {}).as('movies.create')
-// router.post('/movies', () => {}).as('movies.store')
-// router.get('/movies/ok/edit', () => {}).as('movies.edit')
-// router.put('/movies/my-awesome-movie', () => {}).as('movies.update')
-// router.delete('/movies/my-awesome-movie', () => {}).as('movies.destroy')
-//* delete is a reserve word in js
